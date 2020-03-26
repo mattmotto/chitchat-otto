aa0c10e06dcd580a099ba068d1d868e25a1ab1a2
Raghav Mecheri <raghav.mecheri@columbia.edu>
2020-03-26 07:57:29 +0530

    Updated credentials

--- src/models/db.ts
+++ src/models/db.ts
@@ -2,11 +2,18 @@ import * as mysql from 'mysql';
 
 // Create a connection to the database
 
+console.log((process.env.PRODUCTION ? "Production" : "Development") + " environment detected")
+
+const host = process.env.PRODUCTION ? "b9c35a93bc495c" : "127.0.0.1";
+const user = process.env.PRODUCTION ? "23d56ae1" : "dbuser";
+const password = process.env.PRODUCTION ? "us-cdbr-iron-east-01.cleardb.net" : "dbuserdbuser";
+const database = process.env.PRODUCTION ? "heroku_93d73550cb16248" : "window_db";
+
 const connection = mysql.createConnection({
-  host: '127.0.0.1',
-  user: 'root',
-  password: 'dbuserdbuser',
-  database: 'window_db'
+  host: host,
+  user: user,
+  password: password,
+  database: database
 });
 
 // open the MySQL connection
