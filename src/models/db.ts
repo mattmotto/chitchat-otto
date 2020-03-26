afac82543d49d426bc2685f9ff915e25d9a13f73
Raghav Mecheri <raghav.mecheri@columbia.edu>
2020-03-26 11:28:59 +0530

    Small DB fix

--- src/models/db.ts
+++ src/models/db.ts
@@ -6,22 +6,16 @@ var connection = null;
 
 if(process.env.CLEARDB_DATABASE_URL) {
   console.log("Connecting to: "+process.env.CLEARDB_DATABASE_URL);
-  connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
+  connection = mysql.createPool(process.env.CLEARDB_DATABASE_URL);
 } else {
   console.log("Connecting to localhost DB")
-  connection = mysql.createConnection({
+  connection = mysql.createPool({
     host: "127.0.0.1",
     user: "dbuser",
     password: "dbuserdbuser",
     database: "window_db"
   });
 }
-
-// open the MySQL connection
-connection.connect(error => {
-  if (error) throw error;
-  console.log("Successfully connected to the database");
-});
 
 module.exports = connection;
 
